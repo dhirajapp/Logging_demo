35a17fa88741562b5a530e0071bc7df70b553dd7
dhirajapp <dhirajkumar31298@gmail.com>
2022-11-19 09:01:42 +0530

    implement logging concept as manual

--- LoggingAppApplication/src/main/java/com/dhirajapp/dao/ReportDao.java
+++ LoggingAppApplication/src/main/java/com/dhirajapp/dao/ReportDao.java
@@ -1,9 +1,17 @@
 package com.dhirajapp.dao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.dhirajapp.services.MessageService;
+
 public class ReportDao {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ReportDao.class);
 	
 	public String getName(Integer id) {
 		String name = null;
+		logger.info("getName() - method start");
 		if (id == 101) {
 			name = "Dhiraj";
 		} else if (id == 102) {
@@ -11,7 +19,7 @@ public class ReportDao {
 		} else {
 			name = "Invalid Id";
 		}
-	
+		logger.info("getName() - method end");
 		return name;
 	}
 
--- LoggingAppApplication/src/main/java/com/dhirajapp/services/MessageService.java
+++ LoggingAppApplication/src/main/java/com/dhirajapp/services/MessageService.java
@@ -1,16 +1,27 @@
 package com.dhirajapp.services;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+//import ch.qos.logback.classic.Logger;
+
 public class MessageService {
 	
-	
+	private static final Logger logger = LoggerFactory.getLogger(MessageService.class);
 	public String getWelcomeMsg() {
+		logger.debug("getWelcomeMsg() - method start");
 		String msg="Welcome Dhiraj Kumar";
+		
+		logger.debug("getWelcomeMsg() - method end");
 		return msg;
 	}
 
 	
 	public String getGreetMsg() {
+		logger.debug("getGreetMsg() - method start");
+		
 		String msg="Good Morning ..!";
+		logger.debug("getGreetMsg() - method end");
 		return msg;
 	}
 	
